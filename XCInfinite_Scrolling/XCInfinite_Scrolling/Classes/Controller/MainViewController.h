9fd1bf226d36ca9b608fa627bd6520d6e692fbf7
xiaocai <tycai1@sina.com>
2016-06-06 22:34:49 +0800

    初始化控制器

---
+++ XCInfinite_Scrolling/XCInfinite_Scrolling/Classes/Controller/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  XCInfinite_Scrolling
+//
+//  Created by 小蔡 on 16/6/6.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ XCInfinite_Scrolling/XCInfinite_Scrolling/Classes/Controller/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  XCInfinite_Scrolling
+//
+//  Created by 小蔡 on 16/6/6.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
